a87315b417f26954c4e48656792510a6b19ac9dd
gabyguedzu <zooiv3rde@gmail.com>
2018-10-28 22:02:20 -0300

    finalizando conceito sobre instancia e referencia

--- ByteBank/src/CriaConta.java
+++ ByteBank/src/CriaConta.java
@@ -2,18 +2,12 @@ public class CriaConta {
 
     public static void main(String[] args) {
 
-        Conta conta = new Conta();
-        conta.saldo = 200;
-        System.out.println(conta.saldo);
+        Cliente gabriela = new Cliente();
+        gabriela.setNome("Gabriela Rayssa");
+        gabriela.setCpf("058.564.871-92");
+        gabriela.setProfissao("Desenvolvedora Web e Mobile");
 
-        conta.saldo += 100;
-        System.out.println(conta.saldo);
-
-        Conta segundaConta = new Conta();
-        segundaConta.saldo = 1300;
-
-        System.out.println("Primeira conta têm " + conta.saldo);
-        System.out.println("Segunda conta têm " + segundaConta.saldo);
+        Conta conta =  new Conta(34, 8989898);
 
     }
 }
