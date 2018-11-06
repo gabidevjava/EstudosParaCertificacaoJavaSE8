fec89e72b80809eb26d6129084a403cdc28f4a8e
gabyguedzu <zooiv3rde@gmail.com>
2018-11-05 23:09:47 -0200

    heranca, comecando com poliforfismo

---
+++ ByteBankHerdado/src/Gerente.java
@@ -0,0 +1,29 @@
+public class Gerente extends Funcionario{
+
+    private int senha;
+
+    public Gerente() {
+    }
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getbonificacao() {
+        return super.getbonificacao() + super.getSalario();
+    }
+
+    public int getTsenha() {
+        return senha;
+    }
+
+    public void setTsenha(int senha) {
+        this.senha = senha;
+    }
+
+}
+
