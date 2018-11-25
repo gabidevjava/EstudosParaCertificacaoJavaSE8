bb1d4544639af6348c3c7d5cfbd650b21730c1e4
gabyguedzu <zooiv3rde@gmail.com>
2018-11-24 23:19:23 -0200

    mais de classes abstratas II

--- ByteBank/src/Conta.java
+++ ByteBank/src/Conta.java
@@ -1,6 +1,6 @@
-public class Conta {
+public abstract class Conta {
 
-    private double saldo;
+    protected double saldo;
     private int agencia;
     private int numero;
     private Cliente titular;
@@ -13,9 +13,7 @@ public class Conta {
         System.out.println("Estou criando uma conta");
     }
 
-    public void deposita(double valor) {
-        this.saldo +=  valor;
-    }
+    public abstract void deposita(double valor);
 
     public boolean saca(double valor) {
         if (this.saldo >= valor) {
