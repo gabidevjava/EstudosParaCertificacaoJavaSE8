3d34c64399ac2c9bc30f30078382c8cb0e8f129c
gabyguedzu <zooiv3rde@gmail.com>
2018-12-13 23:17:41 -0200

    finalizando a secao sobre exceptions

--- JavaPilha/src/FluxoComTratamento.java
+++ JavaPilha/src/FluxoComTratamento.java
@@ -1,12 +1,12 @@
 public class FluxoComTratamento {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MinhaException {
         System.out.println("Ini do main");
         metodo1();
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws MinhaException {
         System.out.println("Ini do metodo1");
         try {
             metodo1();
