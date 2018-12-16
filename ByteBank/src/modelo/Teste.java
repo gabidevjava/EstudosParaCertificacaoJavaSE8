845f02d0afe13119a178a8630b64c944812fe9b9
gabyguedzu <zooiv3rde@gmail.com>
2018-12-16 20:03:25 -0200

    finalizand a secao de java lang

---
+++ ByteBank/src/modelo/Teste.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ByteBank/src/modelo/TesteString.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class TesteString {
+
+    public static void main(String[] args) {
+
+        String nome = "Gabriela";
+        String outra = nome.replace("G", "GH");
+        //nome.toLowerCase();
+        System.out.println(outra);
+    }
+}
