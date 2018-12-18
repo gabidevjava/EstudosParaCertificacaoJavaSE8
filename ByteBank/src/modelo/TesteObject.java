68f60eb732576fc26d20b19f167911229aff4c9c
gabyguedzu <zooiv3rde@gmail.com>
2018-12-17 22:39:40 -0200

    iniciando com arrays

---
+++ ByteBank/src/modelo/TesteObject.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class TesteObject {
+
+    public static void main(String[] args) {
+        int[] idades = new int[5];
+
+        for(int i = 0; i < idades.length; i++) {
+            idades[i] = i * i;
+        }
+
+        for(int i = 0; i < idades.length; i++) {
+            System.out.println(idades[i]);
+        }
+
+    }
+
+}
