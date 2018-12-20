a8292ff1788043befeaec1e0da85f96ef38d59a8
gabyguedzu <zooiv3rde@gmail.com>
2018-12-19 22:52:23 -0200

    teste de heranca e tipos

---
+++ ByteBank/src/modelo/teste/TesteHera.java
@@ -0,0 +1,16 @@
+package modelo.teste;
+
+import teste.Filho;
+import teste.Pai;
+
+public class TesteHera {
+
+    public static void main(String[] args) {
+
+        Pai pai = new Pai();
+        Filho filho = new Filho();
+
+        //filho = pai;
+
+    }
+}
---
+++ ByteBank/src/teste/Filho.java
@@ -0,0 +1,10 @@
+package teste;
+
+public class Filho extends Pai {
+
+    @Override
+    public String getNome() {
+        return this.nome;
+    }
+
+}
