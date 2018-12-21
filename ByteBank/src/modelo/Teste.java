16775260f22a9eafa70a446af6574a07651a0b84
gabyguedzu <zooiv3rde@gmail.com>
2018-12-20 23:15:04 -0200

    mais de arrays, com adapter

--- ByteBank/src/modelo/Teste.java
+++ ByteBank/src/modelo/Teste.java
@@ -4,5 +4,17 @@ public class Teste {
 
     public static void main(String[] args) {
 
+        GuardadorDeContas guardador = new GuardadorDeContas();
+
+        ContaCorrente cc = new ContaCorrente(11,22);
+        guardador.adiciona(cc);
+
+        int tamanho = guardador.getQuantidadeDeElementos();
+        System.out.println(tamanho);
+
+        Conta ref = guardador.getReferencias(0);
+        System.out.println(ref.getNumero());
+
     }
+
 }
