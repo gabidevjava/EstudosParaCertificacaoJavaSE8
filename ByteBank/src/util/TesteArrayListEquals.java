2d3aee185f58807c234f22ab38095f5661bab3cb
gabyguedzu <zooiv3rde@gmail.com>
2018-12-26 01:03:36 -0200

    sobre o metodo equals

---
+++ ByteBank/src/util/TesteArrayListEquals.java
@@ -0,0 +1,26 @@
+package util;
+
+import modelo.Conta;
+import modelo.ContaCorrente;
+
+import java.util.ArrayList;
+
+public class TesteArrayListEquals {
+
+    public static void main(String[] args) {
+
+        // generics
+        ArrayList<Conta> lista = new ArrayList<Conta>();
+
+        ContaCorrente cc = new ContaCorrente(11, 22);
+        lista.add(cc);
+        ContaCorrente cc1 = new ContaCorrente(11, 22);
+        lista.add(cc1);
+        System.out.println(lista.size());
+
+        Conta ref =  lista.get(0);
+        System.out.println(ref.getNumero());
+
+    }
+
+}
