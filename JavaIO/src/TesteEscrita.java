5c2c75f82c7288ebc1bcbea4ced2042e8aae34de
gabyguedzu <zooiv3rde@gmail.com>
2019-01-02 22:57:05 -0200

    finalizando primeira parte de input e output

--- JavaIO/src/TesteEscrita.java
+++ JavaIO/src/TesteEscrita.java
@@ -4,14 +4,11 @@ public class TesteEscrita {
 
     public static void main(String[] args) throws IOException {
 
-        FileOutputStream file = new FileOutputStream("lorem.txt");
-        Writer writer = new OutputStreamWriter(file);
-        BufferedWriter buffered = new BufferedWriter(writer);
+        FileInputStream fis = new FileInputStream("lorem.txt");
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader bufferedReader = new BufferedReader(isr);
 
-        buffered.write("heeeeeeeeeeeeeeeeeeeeeeeeeeinnnnnnnnnnnnnn");
-        buffered.newLine();
-
-        buffered.close();
+       bufferedReader.close();
 
     }
 
