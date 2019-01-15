357fbf350875b06f4e3c2445b0f06e9d8c7090d9
gabyguedzu <zooiv3rde@gmail.com>
2019-01-14 23:07:20 -0200

    serial version uid

--- JavaIO/src/TesteSerializacao.java
+++ JavaIO/src/TesteSerializacao.java
@@ -4,16 +4,18 @@ public class TesteSerializacao {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        //String nome = "Gabriela Guedes";
+/*        Cliente cliente = new Cliente();
+        cliente.setNome("Gabriela Guedes");
+        cliente.setProfissao("Dev");
 
-        /*ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
-        oos.writeObject(nome);
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.json"));
+        oos.writeObject(cliente);
         oos.close();*/
 
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("objeto.bin"));
-        String nome = (String) ois.readObject();
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.json"));
+        Cliente cliente = (Cliente) ois.readObject();
         ois.close();
-        System.out.println(nome);
+        System.out.println(cliente.getNome());
 
     }
 }
