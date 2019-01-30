3f4363d8b010702710b565ac60c8932a89d2fdff
Gabriela Guedes <zooiv3rde@gmail.com>
2019-01-29 22:02:40 -0200

    voltando pro inicio de array lists

--- GerenciadorDeCursos/src/br/com/alura/TestadorDeCursos.java
+++ GerenciadorDeCursos/src/br/com/alura/TestadorDeCursos.java
@@ -1,14 +1,31 @@
 package br.com.alura;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestadorDeCursos {
 
     public static void main(String[] args) {
-        Curso colecoesDeCursos = new Curso("Java", "Gabriela Guedes");
+
+        String aula1 = "Começando novamente o curso";
+        String aula2 = "Agora vai";
+
+        ArrayList<String> aulas = new ArrayList<>();
+        aulas.add(aula1);
+        aulas.add(aula2);
+
+        System.out.println(aulas);
+        System.out.println("-------------------------------------------------");
+
+        for (String aula : aulas) {
+            System.out.println("Aula " + aula);
+        }
+
+
+   /*     Curso colecoesDeCursos = new Curso("Java", "Gabriela Guedes");
         List<Aula> aulas = colecoesDeCursos.getAulas();
         colecoesDeCursos.adiona((new Aula("de frente com gaby", 46)));
-        System.out.println(aulas);
+        System.out.println(aulas);*/
 
     }
 
