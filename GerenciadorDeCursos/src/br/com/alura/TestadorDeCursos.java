10e96dc3608b233363b5ab8d739c5237b9d53714
Gabriela Guedes <zooiv3rde@gmail.com>
2019-01-30 22:28:17 -0200

    programacao defensiva e collections

--- GerenciadorDeCursos/src/br/com/alura/TestadorDeCursos.java
+++ GerenciadorDeCursos/src/br/com/alura/TestadorDeCursos.java
@@ -1,31 +1,16 @@
 package br.com.alura;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TestadorDeCursos {
 
     public static void main(String[] args) {
 
-        String aula1 = "Começando novamente o curso";
-        String aula2 = "Agora vai";
+       Curso javaColecoes = new Curso("Dominando as coleções", "Gabriela Guedes");
 
-        ArrayList<String> aulas = new ArrayList<>();
-        aulas.add(aula1);
-        aulas.add(aula2);
+       javaColecoes.adiona(new Aula("Dominando map e filter", 30));
+       javaColecoes.adiona(new Aula("Foco na certificação", 20));
+       javaColecoes.adiona(new Aula("Java/JavaScript", 40));
 
-        System.out.println(aulas);
-        System.out.println("-------------------------------------------------");
-
-        for (String aula : aulas) {
-            System.out.println("Aula " + aula);
-        }
-
-
-   /*     Curso colecoesDeCursos = new Curso("Java", "Gabriela Guedes");
-        List<Aula> aulas = colecoesDeCursos.getAulas();
-        colecoesDeCursos.adiona((new Aula("de frente com gaby", 46)));
-        System.out.println(aulas);*/
+       System.out.println(javaColecoes.getAulas());
 
     }
 
