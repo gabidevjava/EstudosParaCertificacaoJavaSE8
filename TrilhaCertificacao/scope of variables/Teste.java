bab49dfa94cd734b6780139776acacce91775820
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-01 19:02:17 -0300

    escipo de while, for, if, blocos

---
+++ TrilhaCertificacao/scope of variables/Teste.java
@@ -0,0 +1,17 @@
+public class Teste {
+	
+	public void m1() {
+		for(int i = 0, j = 0; i <= 10; i++) j++; {
+				
+			}
+			
+			System.out.println(i);
+			System.out.println(j);
+	}
+	
+	public static void main(String[] args) {
+		Teste teste = new Teste();
+		teste.m1();
+	
+	}
+}
