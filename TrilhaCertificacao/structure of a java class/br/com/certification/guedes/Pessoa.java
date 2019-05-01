488c663ce24d7ac35c93cd132da8f5ce1ccf7a67
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-01 20:21:45 -0300

    structure of a java class

---
+++ TrilhaCertificacao/structure of a java class/br/com/certification/guedes/Pessoa.java
@@ -0,0 +1,15 @@
+package br.com.certification.guedes;
+
+interface Figuras {
+//por padrao
+public static final int TAMANHO = 10;
+
+//metodo por padrao é abstrato
+public abstract
+
+}
+
+class Pessoa {
+
+
+}
