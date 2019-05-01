6c1a79ba7c3b03f96514ccd4a71054e277af8915
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-01 19:13:52 -0300

    falando de variabeis e metodos static

--- TrilhaCertificacao/scope of variables/Teste.java
+++ TrilhaCertificacao/scope of variables/Teste.java
@@ -1,17 +1,12 @@
+class Pessoa {
+	static int id = 1;
+
+}
+
 public class Teste {
 	
-	public void m1() {
-		for(int i = 0, j = 0; i <= 10; i++) j++; {
-				
-			}
-			
-			System.out.println(i);
-			System.out.println(j);
-	}
-	
 	public static void main(String[] args) {
-		Teste teste = new Teste();
-		teste.m1();
+		System.out.println(Pessoa.id);
 	
 	}
 }
