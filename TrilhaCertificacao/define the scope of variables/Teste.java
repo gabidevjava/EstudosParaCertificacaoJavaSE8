3456b648a3529b7e306328bc9e6a764655788b78
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-04 14:32:40 -0300

    define the scope of variables fix

--- TrilhaCertificacao/define the scope of variables/Teste.java
+++ TrilhaCertificacao/define the scope of variables/Teste.java
@@ -1,7 +1,10 @@
 public class Teste {
 
-	public static void main() {
-	
+	public static void main(String[] args) {
+		boolean BOOLEAN = false;
+		if(BOOLEAN) {
+			System.out.println("yes");
+		}
 	}
 
 }
