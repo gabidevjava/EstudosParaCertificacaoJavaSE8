86a7c575ccfd6e42b5b6c6cf9cb0f4ecbb6d0b2c
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-05 18:57:27 -0300

    string build e buffer

---
+++ TrilhaCertificacao/creating and manipulating strings/TestaStringBuilderEBuffer.java
@@ -0,0 +1,13 @@
+public class TestaStringBuilderEBuffer {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer("caelum");
+		sb.append(" - ");
+		sb.append("teste now");
+		System.out.println(sb.toString());
+		
+		
+	}
+
+}
