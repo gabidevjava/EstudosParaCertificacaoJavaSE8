5771bd290dacaa69372979d401cec53a7b95c2dd
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-08 22:17:55 -0300

    short circuitr

---
+++ TrilhaCertificacao/operators boolean/TestaOperadores.java
@@ -0,0 +1,15 @@
+public class TestaOperadores {
+
+	public static boolean metodo(String msg) {
+		System.out.println(msg);
+		return true;
+	
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(1 == 1 && 1 > 2);
+		System.out.println(1 == 1 || 1 > 2);
+	
+	}
+
+}
