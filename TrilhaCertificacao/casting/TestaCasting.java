1c0c468953a00146a936ccd7ea26db0d85eae12c
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-09 21:29:37 -0300

    tests

--- TrilhaCertificacao/casting/TestaCasting.java
+++ TrilhaCertificacao/casting/TestaCasting.java
@@ -3,6 +3,14 @@ public class TestaCasting {
 	public static void main(String[] args) {
 		double a = 15.3;
 		int b = (int) a;
+		
+		int i;
+        for (i = 0; i < 5; i++) {
+            if (++i % 3 == 0) {
+                break;
+            }
+        }
+        System.out.println(i);
 	
 	}
 
