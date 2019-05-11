96612f640587df7dd9a12d26373285f3a2793f62
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-11 14:09:35 -0300

    equals ==

---
+++ TrilhaCertificacao/compare objects/Teste.java
@@ -0,0 +1,14 @@
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		String nome1 = new String("Gabriela");
+		String nome2 = new String("Gabriela");
+		
+		String nome2 = "Gabriela";
+						
+		System.out.println(nome1 == nome2);
+	
+	}
+
+}
