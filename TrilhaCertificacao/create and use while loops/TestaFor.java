bc1792f22d9ea5fd3f2d9d0fad4f6a0c370343cb
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-13 22:30:35 -0300

    for and enhanced for loop

---
+++ TrilhaCertificacao/create and use while loops/TestaFor.java
@@ -0,0 +1,17 @@
+public class TestaFor {
+	
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	
+	}
+
+}
