b0d82bb026201b531ca1783fa4150ca4feeb328c
Gabriela Guedes <zooiv3rde@gmail.com>
2019-05-15 23:55:32 -0300

    create methods

--- TrilhaCertificacao/create methods with arguments and return values/TestaMetodos.java
+++ TrilhaCertificacao/create methods with arguments and return values/TestaMetodos.java
@@ -1,3 +1,15 @@
+class A {
+    void nada(int i) {
+        if(i >= 0) {
+            return;
+
+            // não compila, pois nunca chegará aqui
+            System.out.println("era positivo ou zero"); 
+        }
+        System.out.println("negativo");
+    }
+}
+
 public class TestaMetodos {
 
 	public static void main(String[] args) {
