b017dc67a1f3e03257d33c72598d1bcba769d093
Gabriela Guedes <zooiv3rde@gmail.com>
2019-06-03 22:21:32 -0300

    excersis

--- TestesCertificacao/src/main/BemVindo.java
+++ TestesCertificacao/src/main/BemVindo.java
@@ -1,10 +1,89 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BemVindo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		long l = 100_00l;
+		double d = 10_0.35d;
+		System.out.println(d);*/
+		
+	       /* final String fName = "James";
+	        String lName = "Gosling";
+	        String name1 = fName + lName;
+	        String name2 = fName + "Gosling";
+	        String name3 = "James" + "Gosling";
+	        System.out.println(name1 == name2);
+	        System.out.println(name2 == name3);*/
+		
+		
+	         //System.out.println(new Boolean("ture"));
+	   
+		 /* String [] arr = new String[7];
+	         System.out.println(arr);*/
+		/*
+		 List<String> dryFruits = new ArrayList<>();
+         dryFruits.add("Walnut");
+         dryFruits.add("Apricot");
+         dryFruits.add("Almond");
+         dryFruits.add("Date");
+ 
+         ListIterator<String> iterator = dryFruits.listIterator();
+         while(iterator.hasNext()) {
+             if(iterator.next().startsWith("A")) {
+                 iterator.remove();
+              }
+         }
+ 
+         System.out.println(dryFruits);*/
+		
+		/*List<Integer> list = new ArrayList<>();
+        list.add(100);
+        list.add(200);
+        list.add(100);
+        list.add(200);
+        list.remove(new Integer(100));
 
+        System.out.println(list);*/
+		
+		 /*Boolean b1 = new Boolean("tRuE");
+         Boolean b2 = new Boolean("fAlSe");
+         Boolean b3 = new Boolean("abc");
+         Boolean b4 = null;
+         System.out.println(b1 + ":" + b2 + ":" + b3 + ":" + b4);*/
+		
+		/*LocalDate newYear = LocalDate.of(2018, 1, 1);
+        LocalDate eventDate = LocalDate.of(2018, 1, 1);
+        boolean flag1 = newYear.isAfter(eventDate);
+        boolean flag2 = newYear.isBefore(eventDate);
+        System.out.println(flag1 + ":" + flag2);*/
+	    
+		/* List<String> list = new ArrayList<>();
+         list.add(0, "Array");
+         list.add(0, "List");
+ 
+         System.out.println(list);*/
+		
+//		 List<String> list = new ArrayList<>();
+//         list.add("ONE");
+//         list.add("TWO");
+//         list.add("THREE");
+//         list.add("THREE");
+// 
+//         if(list.remove(2)) {
+//             list.remove("THREE");
+//         }
+// 
+//         System.out.println(list);
+		
+	         /*System.out.println("Output is: " + (10 != 5));*/
+		
+		 System.out.println(args.length);
+	    
+	     
 	}
 
 }
