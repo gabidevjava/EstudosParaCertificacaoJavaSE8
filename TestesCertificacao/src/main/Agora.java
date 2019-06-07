20fe80b41d9c0e4cf24d6a8c11d4fa035d78a6d8
Gabriela Guedes <zooiv3rde@gmail.com>
2019-06-06 21:25:36 -0300

    excer

--- TestesCertificacao/src/main/Agora.java
+++ TestesCertificacao/src/main/Agora.java
@@ -7,15 +7,12 @@ public class Agora {
 	
 	public static void main(String[] args) {
 		
-		int start = 1;
-        int sum = 0;
-        do {
-            if(start % 2 == 0) {
-                continue;
-            }
-            sum += start;
-        } while(++start <= 10);
-        System.out.println(sum);
+		int [] arr1 = {1, 2, 3};
+        int [] arr2 = {'A', 'B'}; //ASCII code of 'A' is 65, 'B' is 66
+        arr1 = arr2;
+        for(int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
     
 		
 		/*final int i1 = 1;
