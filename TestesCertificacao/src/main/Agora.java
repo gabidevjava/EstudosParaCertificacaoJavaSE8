29e2dae80a9522ff6e5c9067d453f987af84a117
Gabriela Guedes <zooiv3rde@gmail.com>
2019-06-09 20:15:11 -0300

    substring test

--- TestesCertificacao/src/main/Agora.java
+++ TestesCertificacao/src/main/Agora.java
@@ -1,17 +1,33 @@
 package main;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Agora {
 	
 	public static void main(String[] args) {
+		String [] arr = {"A", "ab", "bab", "Aa", "bb", "baba", "aba", "Abab"};
 		
-		StringBuilder sb = new StringBuilder(100);
-        System.out.println(sb.length() + ":" + sb.toString().length());
-		
+		String beijo = "beijo";
+		boolean equals = beijo.toUpperCase().substring(0,1).equals("b");
+		System.out.println(beijo);
+		System.out.println(equals);
+		System.out.println("-----------------------------------------------");
+		 
+        Predicate<String> p = s -> s.toUpperCase().substring(0,1).equals("A");
+
+        processStringArray(arr, p);
+     
 	}
+	
+    private static void processStringArray(String [] arr, 
+            Predicate<String> predicate) {
+			for(String str : arr) {
+			if(predicate.test(str)) {
+			System.out.println(str);
+			}
+			}
+			}
 	
 }
