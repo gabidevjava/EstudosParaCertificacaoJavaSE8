29fe1a4a9726a266e2f986ee88745547a01efcb5
Gabriela Guedes <zooiv3rde@gmail.com>
2019-06-09 19:29:45 -0300

    simulationa

--- TestesCertificacao/src/main/Agora.java
+++ TestesCertificacao/src/main/Agora.java
@@ -1,22 +1,17 @@
 package main;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Agora {
 	
 	public static void main(String[] args) {
-		 List<Boolean> list = new ArrayList<>();
-         list.add(true);
-         list.add(new Boolean("tRue"));
-         list.add(new Boolean("abc"));
- 
-         if(list.remove(1)) {
-             list.remove(1);
-         }
- 
-         System.out.println(list);
+		
+		StringBuilder sb = new StringBuilder(100);
+        System.out.println(sb.length() + ":" + sb.toString().length());
 		
 	}
-
+	
 }
